e709917525715b9183e18ea6b94a87d8c145b233
Paul Walsh <twentytwentyk@live.com>
2017-08-10 12:38:36 -0400

    artist name

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -9,10 +9,21 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View></View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  }
 };
 
 export default AlbumDetail;
