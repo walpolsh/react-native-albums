251a4a03fc4a6c0114b3897c60b98360861b9ab4
Paul Walsh <twentytwentyk@live.com>
2017-08-11 13:33:25 -0400

    onPress

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
   return (
-    <Text>
+    <TouchableOpacity onPress={() => console.log('pressed!')} style={buttonStyle}>
+      <Text style={textStyle}>
       Click me!!!
-    </Text>
+      </Text>
+    </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5,
+  },
 };
 
 // TouchableHighlight
@@ -16,10 +40,10 @@ const Button = () => {
 // A button
 
 // TouchableOpacity
-// A wrapper for making views respond properly to touches. 
+// A wrapper for making views respond properly to touches.
 // On press down, the opacity of the wrapped view is decreased,
 // dimming it.
 // Opacity is controlled by wrapping the children in an Animated.View, which is added to the view hiearchy. Be aware that this can affect layout.
-
+// A button
 
 export default Button;
